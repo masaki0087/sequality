0cf0e856e22a3e3380d2f66961707710375f2024
masaki0087 <nabesan0087@gmail.com>
2026-07-06 10:47:50 +0900

    create test method

--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -5,9 +5,38 @@ import static org.junit.Assert.*;
 
 public class CalculateTest {
   @Test
-  public void testSum() {
+  public void testGetIndividualFee_10() {
     Calculate calculate = new Calculate();
-    int expected = 5;
-    assertEquals(expected, calculate.sum(2, 3));
+    int expected = 1000;
+    assertEquals(expected, calculate.getIndividualFee(10));
   }
+
+  @Test
+  public void testGetIndividualFee_25() {
+    Calculate calculate = new Calculate();
+    int expected = 2000;
+    assertEquals(expected, calculate.getIndividualFee(25));
+  }
+
+  @Test
+  public void testGetTotalFee_10_1() {
+    Calculate calculate = new Calculate();
+    int expected = 1000;
+    assertEquals(expected, calculate.getTotalFee(10, 1));
+  }
+
+  @Test
+  public void testGetTotalFee_25_1() {
+    Calculate calculate = new Calculate();
+    int expected = 2000;
+    assertEquals(expected, calculate.getTotalFee(25, 1));
+  }
+
+  @Test
+  public void testGetTotalFee_10_15() {
+    Calculate calculate = new Calculate();
+    int expected = 2700;
+    assertEquals(expected, calculate.getTotalFee(10, 15));
+  }
+
 }
